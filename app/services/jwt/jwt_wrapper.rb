2bcbf89f0a6ab71ffe9bd37acffe372c04c19c77
Jesus Diaz <jadiaz.inf@gmail.com>
2025-06-14 00:15:36 -0400

    production

--- app/services/jwt/jwt_wrapper.rb
+++ app/services/jwt/jwt_wrapper.rb
@@ -1,6 +1,7 @@
 class JWTWrapper
   def self.decode(token)
-    secret_key = Rails.application.credentials.jwt.secret
+    # secret_key = Rails.application.credentials.jwt.secret
+    secret_key = ENV.fetch("JWT_SECRET")
 
     begin
       decoded_token = JWT.decode(token, secret_key, true, algorithm: 'HS256')
