ba5c1d1c6cc30b2480290b903119dd9448214aa0
Jesus Diaz <jadiaz.inf@gmail.com>
2025-06-14 02:06:32 -0400

    holis

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root to: "home#index"
   namespace :api do
     namespace :v1 do
       resources :users do
