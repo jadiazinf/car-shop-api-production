66d26ee933f13fa3aedea2af8e75f22fd5b5f471
Jesus Diaz <jadiaz.inf@gmail.com>
2025-06-18 20:40:53 -0400

    jesus debug part 2

--- app/services/jwt/jwt_wrapper.rb
+++ app/services/jwt/jwt_wrapper.rb
@@ -1,17 +1,7 @@
 module Jwt
   class JwtWrapper
     def self.decode(token)
-      # secret_key = Rails.application.credentials.jwt.secret
-
-      # secret_key = if Rails.application.credentials.SECRET_KEY_BASE || Rails.application.credentials.secret_key_base
-      #           Rails.application.credentials.jwt.secret
-      #         else
-      #           ENV.fetch("SECRET_KEY_BASE")
-      #         end
-
-      # secret_key = ENV['JWT_SECRET_KEY']
-
-      secret_key = Rails.application.credentials[:jwt]&.fetch(:secret)
+      secret_key = ENV.fetch('DEVISE_JWT_SECRET_KEY')
 
       begin
         decoded_token = JWT.decode(token, secret_key, true, algorithm: 'HS256')
